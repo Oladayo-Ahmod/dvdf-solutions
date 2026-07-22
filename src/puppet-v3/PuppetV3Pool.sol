0d8f14a3666fb3bc064ae748b91fa8f767f32b24
blockend_dev <oladayoahmod112@gmail.com>
2026-07-22 14:26:59 +0100

    solve challenge 14, puppet-v3

--- src/puppet-v3/PuppetV3Pool.sol
+++ src/puppet-v3/PuppetV3Pool.sol
@@ -60,6 +60,7 @@ contract PuppetV3Pool {
 
     function _getOracleQuote(uint128 amount) private view returns (uint256) {
         (int24 arithmeticMeanTick,) = OracleLibrary.consult({pool: address(uniswapV3Pool), secondsAgo: TWAP_PERIOD});
+        
         return OracleLibrary.getQuoteAtTick({
             tick: arithmeticMeanTick,
             baseAmount: amount,
